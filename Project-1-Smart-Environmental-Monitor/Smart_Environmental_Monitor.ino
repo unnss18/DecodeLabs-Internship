0979689bd74d172fe3a4e0cd5a2bc3741ea8cc68
unnss18 <149695619+unnss18@users.noreply.github.com>
2026-07-15 22:00:31 +0530

    Add Smart Environmental Monitor Arduino code

---
+++ Project-1-Smart-Environmental-Monitor/Smart_Environmental_Monitor.ino
@@ -0,0 +1,39 @@
+#include <DHT.h>
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Smart Environmental Monitor");
+  dht.begin();
+}
+
+void loop()
+{
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  if (isnan(humidity) || isnan(temperature))
+  {
+    Serial.println("Failed to read from DHT sensor!");
+    delay(2000);
+    return;
+  }
+
+  Serial.println("----------------------------");
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println(" °C");
+
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.println(" %");
+
+  Serial.println("----------------------------");
+
+  delay(2000);
+}
